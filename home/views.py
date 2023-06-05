a226443fb97ef941b1f8c7482b7583045be06ef4
zhra <z.sarami81@gmail.com>
2023-06-05 20:05:45 +0330

    patch method to update blog adde to  BligView

--- home/views.py
+++ home/views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Blog
 from django.db.models import Q
+
+
 class BlogView(APIView) :
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
@@ -57,4 +59,46 @@ class BlogView(APIView) :
                 } , status = status.HTTP_400_BAD_REQUEST )
 
 
+    def patch(self , request) :
+        try:
+            data = request.data  
+            blog = Blog.objects.filter(uid= data.get('uid'))
+
+            if not blog.exists():
+                return Response({
+                    'data' : {} ,
+                    'message' : 'invalid blog uid' ,
+                } , status = status.HTTP_400_BAD_REQUEST )
+
+
+            print(1)
+            if request.user != blog[0].user :
+                return Response({
+                    'data' : {} ,
+                    'message' : 'you are nor authorize to do this' ,
+                } , status = status.HTTP_400_BAD_REQUEST )
+            print(2)
+            serializer = BlogSerializer(blog[0], data=data ,  partial= True)
+            
+            if not serializer.is_valid():
+                return Response({
+                    'data' : serializer.errors ,
+                    'message' : 'something went wrong' ,
+                } , status = status.HTTP_400_BAD_REQUEST )
+            serializer.save()
+            
+            return Response({
+                'data' : serializer.data ,
+                'message' : 'blog is updated seccesfully' ,
+            } , status= status.HTTP_201_CREATED)
         
+        
+        except Exception as e :
+            print(e)
+            return Response({
+                    'data' : {} ,
+                    'message' : 'something went wrong' ,
+                } , status = status.HTTP_400_BAD_REQUEST )
+            
+            
+            
